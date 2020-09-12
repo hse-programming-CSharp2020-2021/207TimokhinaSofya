ed3a29316a7f68fa96ec9d25b6e2c9aae310be3b
Софья Тимохина <sprh.t@MacBook-Pro-Sofa.local>
2020-09-12 15:56:12 +0300

    Task 3 solution

---
+++ module1/seminar1_03/Task3/Task3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static double functuon(double i)
+        {
+            return Math.Pow(i, 2);
+        }
+        static void Main(string[] args)
+        {
+            double area = 0;
+            if (!(double.TryParse(Console.ReadLine(), out double delta) &&
+                double.TryParse(Console.ReadLine(), out double A)))
+            {
+                Console.WriteLine("Проверьте введенные данные!");
+            }
+            else
+            {
+                A /= delta;
+                while (A > 0) {
+                    area += delta * (functuon(A * delta) + functuon((A - 1) * delta)) / 2;
+                    A -= 1;
+                }
+                Console.WriteLine(area);
+            }
+        }
+    }
+}
