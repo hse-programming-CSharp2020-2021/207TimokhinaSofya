4065ddee0f473b6d5f120fbf40aecf2f180be942
Софья Тимохина <sprh.t@MacBook-Pro-Sofa.local>
2020-09-12 15:17:48 +0300

    Task 1 sol

---
+++ module1/seminar1_03/Task1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static string Checkmark(int mark)
+        {
+            switch (mark)
+            {
+                case 1:
+                case 2:
+                case 3:
+                    return "Неудовлетворительно";
+                case 4:
+                case 5:
+                    return "Удовлетворительно";
+                case 6:
+                case 7:
+                    return "Хорошо";
+                case 8:
+                case 9:
+                case 10:
+                    return "Отлично";
+            }
+            return "Вы ошиблись";
+        }
+        static void Main(string[] args)
+        {
+            int mark;
+            if (int.TryParse(Console.ReadLine(), out mark)) {
+                Console.WriteLine(Checkmark(mark));
+            }
+            else
+            {
+                Console.WriteLine("Ошибка");
+            
+            }
+        }
+    }
+}
