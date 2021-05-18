eb69218c822ee1519462e4083b62705eb6f08f65
sprh.t <sprh.tt@gmail.com>
2021-05-18 19:00:08 +0300

    Sem 6

---
+++ module4/seminar6/Task1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+//В некоторой коллекции хранятся целые числа. Вводите n с клавиатуры и генерируете последовательность от -10000 до 10000.
+//Все выражения составить в форме запросов и в форме методов расширений.
+//1) Составить LINQ-выражение, формирующее коллекцию, содержащую их последние цифры.
+//2) Сгруппировать коллекцию по последней цифре.
+//3) Составить запрос, формирующий коллекцию четных положительных чисел и выводит их количество
+//4) Составить запрос, находящий сумму всех четных чисел
+//5) Составить запрос, который сортирует числа по 1 и по последней цифре числа
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            var seq = Enumerable.Range(1, random.Next(8, 13)).Select(el => el = random.Next(-10000, 10000)).ToList();
+            Console.WriteLine($"seq: {string.Join(", ", seq)}\r\n");
+            LastNums(seq);
+            GroupByLast(seq);
+            Evens(seq);
+            EvensSum(seq);
+            Sort(seq);
+        }
+
+        static void LastNums(List<int> seq)
+        {
+            var last1 = seq.Select(el => Math.Abs(el % 10));
+            var last2 = from el in seq
+                        select Math.Abs(el % 10);
+            Console.WriteLine($"last1: {string.Join(", ", last1)}");
+            Console.WriteLine($"last2: {string.Join(", ", last2)}\r\n");
+        }
+
+        static void GroupByLast(List<int> seq)
+        {
+            var group1 = seq.GroupBy(el => el % 10).Select(group => $"{group.Key}, nums: {string.Join(", ", group)}");
+            var group2 = from el in seq
+                         group el by el % 10 into gr
+                                             select $"{gr.Key}, nums: {string.Join(", ", gr)}";
+            Console.WriteLine($"group1: \r\n{string.Join("\r\n", group1)}");
+            Console.WriteLine($"group2: \r\n{string.Join("\r\n", group2)}\r\n");
+        }
+
+        static void Evens(List<int> seq)
+        {
+            var evens1 = seq.Where(el => el % 2 == 0 && el > 0).Count();
+            var evens2 = (from el in seq
+                         where el % 2 == 0 && el > 0
+                         select el).Count();
+            Console.WriteLine($"evens1 {evens1}");
+            Console.WriteLine($"evens2 {evens2}\r\n");
+        }
+
+        static void EvensSum(List<int> seq)
+        {
+            var evens1 = seq.Where(el => el % 2 == 0).Sum();
+            var evens2 = (from el in seq
+                          where el % 2 == 0
+                          select el).Sum();
+            Console.WriteLine($"evens1 {evens1}");
+            Console.WriteLine($"evens2 {evens2}\r\n");
+        }
+
+        static void Sort(List<int> seq)
+        {
+            var sort1 = seq.OrderBy(el => Math.Abs(el / Math.Pow(10, (int)Math.Log10(el)))).ThenBy(el => Math.Abs(el % 10));
+            var sort2 = from el in seq
+                        orderby Math.Abs(el / Math.Pow(10, (int)Math.Log10(el))), Math.Abs(el % 10)
+                        select el;
+            Console.WriteLine($"sort1: {string.Join(", ", sort1)}");
+            Console.WriteLine($"sort1: {string.Join(", ", sort1)}\r\n");
+        }
+    }
+}
---
+++ module4/seminar6/Task2/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Random random = new Random();
+            Zoo zoo = new Zoo(Enumerable.Range(1, n).Select(el => GetAnimal()).ToList());
+            zoo.Animals.ForEach(Console.WriteLine);
+            Console.WriteLine(Environment.NewLine);
+            zoo.Animals.ForEach(an => an.DoSound());
+            Console.WriteLine(Environment.NewLine);
+            zoo.Animals.Where(an => an is Bird && an.IsTakenCare).ToList().ForEach(Console.WriteLine);
+            Console.WriteLine(Environment.NewLine);
+            zoo.Animals.Where(an => an is Mammal && !an.IsTakenCare).ToList().ForEach(Console.WriteLine);
+        }
+
+        private static Animal GetAnimal()
+        {
+            Random random = new Random();
+            switch (random.Next(0, 2)) {
+                case 0:
+                    return new Bird(GetARandomString(), random.Next(0, 2) == 0, random.Next(1, 101));
+                 case 1:
+                    return new Mammal(GetARandomString(), random.Next(0, 2) == 0, random.Next(1, 20));
+                default:
+                    return null;
+            }
+        }
+
+        private static string GetARandomString()
+        {
+            Random random = new Random();
+            var randomString = string.Join("", Enumerable.Range(1, random.Next(6, 12)).Select(x => (char)random.Next('a', 'z')));
+            return char.ToUpper(randomString[0]) + randomString.Substring(1);
+        }
+    }
+
+
+    interface IVocal
+    {
+        public void DoSound();
+    }
+
+    abstract class Animal : IVocal
+    {
+        static public int id = 1;
+        public int Id { get; set; } = 1;
+        public string Name { get; set; }
+        public bool IsTakenCare { get; set; }
+
+        public void DoSound()
+        {
+        }
+
+        public override string ToString() => $"Id: {Id}, name: {Name}, is taken care: {(IsTakenCare ? "yes" : "no")}";
+    }
+
+    class Mammal: Animal
+    {
+        private int paws;
+        public int Paws
+        {
+            get => paws;
+            set
+            {
+                paws = value < 1 || value > 20 ? throw new ArgumentException("Too much") : value;
+            }
+        }
+
+        public Mammal(string name, bool isTakenCare, int paws)
+        {
+            Name = name;
+            IsTakenCare = isTakenCare;
+            Paws = paws;
+            Id = id++;
+        }
+
+        public override string ToString() => $"Mammal. {base.ToString()}, paws: {paws}";
+
+        new public void DoSound()
+        {
+            Console.WriteLine("«я млекопитающие, би-би-би»");
+        }
+    }
+
+    class Bird: Animal
+    {
+        private int speed;
+        public int Speed
+        {
+            get => speed;
+            set
+            {
+                speed = value < 1 || value > 100 ? throw new ArgumentException("Too much") : value;
+            }
+        }
+
+        public Bird(string name, bool isTakenCare, int speed)
+        {
+            Name = name;
+            IsTakenCare = isTakenCare;
+            Speed = speed;
+            Id = id++;
+        }
+
+        public override string ToString() => $"Bird. {base.ToString()}, speed: {speed}";
+    }
+
+    class Zoo
+    {
+        public List<Animal> Animals { get; set; }
+
+        public Zoo(List<Animal> animals)
+        {
+            Animals = animals;
+        }
+
+        new public void DoSound()
+        {
+            Console.WriteLine("я птичка, пип-пип-пип");
+        }
+    }
+}
